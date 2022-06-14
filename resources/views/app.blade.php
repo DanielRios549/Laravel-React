0fd9bcb09588d79d69847bee80961cf3e858dde0
Daniel Rios <danielrios.flamengo@gmail.com>
2022-06-14 10:25:27 -0300

    Add Icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="/images/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script type="module">
             window.__vite_plugin_react_preamble_installed__ = true
