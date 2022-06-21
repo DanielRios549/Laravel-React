7e206665e0ac6d488a3f4feae518ae1dd008c825
Daniel Rios <danielrios.flamengo@gmail.com>
2022-06-21 15:03:12 -0300

    Add PostCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import Autoprefixer from 'autoprefixer'
 import { resolve } from 'path'
 
 export default defineConfig(({ command }) => ({
