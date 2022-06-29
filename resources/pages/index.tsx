6ec37494a0478a0801c2a7956cbc8fdbfc05933a
Daniel Rios <danielrios.flamengo@gmail.com>
2022-06-29 14:47:00 -0300

    Fix Card Array Types

--- resources/pages/index.tsx
+++ resources/pages/index.tsx
@@ -1,14 +1,16 @@
 import Layout from '$/layout/Main'
 import Card from '$/components/Card'
 import type { PageProps } from '$/types/page'
+import type { Items } from '$/types/items'
 
 export default function Home(props: PageProps) {
     const count = props.itemsCount
+    const cards = count && Object.keys(count) as [keyof Items]
 
     return (
         <Layout {...props}>
             <section className="cards">
-                {count && Object.keys(count).map((item, index) => (
+                {cards && cards.map((item, index) => (
                     <Card key={index} name={item}>
                         <p>{count[item]}</p>
                     </Card>
