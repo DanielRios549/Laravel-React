40ef32100dcf2b43c052ccd29db345144737d96d
Daniel Rios <danielrios.flamengo@gmail.com>
2022-07-01 15:04:11 -0300

    Update Styles

--- resources/layout/parts/Header/index.tsx
+++ resources/layout/parts/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Inertia } from '@inertiajs/inertia'
 import { usePage } from '@inertiajs/inertia-react'
 import route from 'ziggy-js'
@@ -8,17 +9,20 @@ export default function Header() {
     const user = useConfig(({ config }) => config.user)
     const { title } = usePage().props as any
 
+    const [userMenu, setUserMenu] = useState(false)
+
     const logout = () => {
         Inertia.delete(route('user/logout'))
     }
 
     return (
         <header className={style.header}>
-            <header>
-                <h2>{title}</h2>
-            </header>
-            <section>
-                <h3>{user?.name}</h3>
+            <h2>{title}</h2>
+            <figure onClick={() => setUserMenu(!userMenu)}>
+                <img src="/images/user.jpg" alt="user image"/>
+            </figure>
+            <section className={userMenu ? 'open' : ''}>
+                <p>{user?.name}</p>
                 <button onClick={logout}>Logout</button>
             </section>
         </header>
